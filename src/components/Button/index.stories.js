31f39bc6912dcd44c74a8cb4c2bcdf41699029b2
Ângelo Lucas <angelo.lrc@gmail.com>
2019-09-16 21:21:23 -0300

    Watch stories inside src/components

--- src/components/Button/index.stories.js
+++ src/components/Button/index.stories.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import Button from './';
 
 export default {
   title: 'Button',
 };
 
-export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const text = () => (
+  <Button onClick={action('clicked')}>Hello Button</Button>
+);
 
 export const emoji = () => (
   <Button onClick={action('clicked')}>
