9e42d452a8809e6d8841cc7fb722577824280146
Ângelo Lucas <angelo.lrc@gmail.com>
2019-09-21 16:14:27 -0300

    Logo storiy inside atoms separator

--- src/components/atoms/logo/logo.stories.js
+++ src/components/atoms/logo/logo.stories.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 import Logo from './logo';
 import doc from './logo.md';
 
-storiesOf('Logo', module).add('default', () => <Logo />, {
+storiesOf('Atoms/Logo', module).add('default', () => <Logo />, {
   notes: doc,
 });
