cb3c0497dee17968d3de0d8aef2c33f9c7afa367
Ângelo Lucas <angelo.lrc@gmail.com>
2019-09-24 23:14:48 -0300

    Button - Remove unnecessary disable props

--- src/components/atoms/button/button.js
+++ src/components/atoms/button/button.js
@@ -2,15 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as S from './button.css';
 
-const Button = ({ children, disabled, ...props }) => (
-  <S.Button disabled={disabled} {...props}>
-    {children}
-  </S.Button>
+const Button = ({ children, ...props }) => (
+  <S.Button {...props}>{children}</S.Button>
 );
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
-  disabled: PropTypes.bool,
 };
 
 export default Button;
