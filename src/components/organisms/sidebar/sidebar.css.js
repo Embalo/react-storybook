857e234b719154bb2ba1a274755714d1b6a58815
Ângelo Lucas <angelo.lrc@gmail.com>
2019-10-03 22:23:57 -0300

    Sidebar component

---
+++ src/components/organisms/sidebar/sidebar.css.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Sidebar = styled.div`
+  box-sizing: border-box;
+  background: white;
+  width: 456px;
+  height: 100%;
+  padding: 60px 96px 58px 120px;
+`;
